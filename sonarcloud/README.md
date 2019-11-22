0b619b8efec572f124b0a1716a2540d069469b5c
eric-therond-sonarsource <eric.therond@sonarsource.com>
2019-11-22 18:20:12 +0100

    more optiosn for sonarcloud

--- sonarcloud/README.md
+++ sonarcloud/README.md
@@ -9,8 +9,11 @@ npm install
 ## Use
 
 ```
-node stats.js type resolution
-where type could be: VULNERABILITY, CODE_SMELL, SECURITY_HOTSPOT or BUG
-where type could be: WONTFIX, FALSE-POSITIVE, FIXED or REMOVED
+node stats.js type resolution statuses fromhotspot
+where rule type could be: VULNERABILITY,CODE_SMELL,SECURITY_HOTSPOT,BUG
+where issue type could be: VULNERABILITY,CODE_SMELL,SECURITY_HOTSPOT,BUG
+where resolution could be: ALL,NOTRESOLVED,WONTFIX,FALSE-POSITIVE,FIXED,REMOVED
+where statuses is optional and could be: ALL,OPEN,CONFIRMED,REOPENED,RESOLVED,CLOSED,TO_REVIEW,IN_REVIEW,REVIEWED
+where fromhotspot is optional and could be: false,true
 ```
 And next open dashboard.html with your favorite internet browser.
--- sonarcloud/stats.js
+++ sonarcloud/stats.js
@@ -8,20 +8,66 @@ function statsIssues(nb, rule) {
   var object = {
     "name": "<a href='https://jira.sonarsource.com/browse/RSPEC-"+rule.rspecnum+"' target=_blank>"+rule.name+"</a>",
     "nbissues": nb,
-    "language": rule.language
+    "language": rule.language,
+    "type": rule.type
   };
   
   stats.push(object);
 }
 
-function fetchIssues(idrule, type, resolution) {
+function fetchIssues(page, idrule, issuetypes, resolution, statuses, fromhotspot = "notdefined", totalsofar = 0) {
   if(typeof rules[idrule] !== "undefined") {
-    fetch('https://sonarcloud.io/api/issues/search?types='+type+'&resolutions='+resolution+'&rules='+rules[idrule].id)
+    
+    if(resolution === "ALL") {
+      var query = 'https://sonarcloud.io/api/issues/search?types='+issuetypes+'&statuses='+statuses+'&rules='+rules[idrule].id;
+    }
+    else if(resolution === "NOTRESOLVED") {
+      var query = 'https://sonarcloud.io/api/issues/search?types='+issuetypes+'&resolved=false&statuses='+statuses+'&rules='+rules[idrule].id;
+    }
+    else {
+      var query = 'https://sonarcloud.io/api/issues/search?types='+issuetypes+'&resolutions='+resolution+'&statuses='+statuses+'&rules='+rules[idrule].id;
+    }
+    
+    if(fromhotspot !== "notdefined") {
+      query += ("&ps=500&p="+page);
+    }
+    
+    fetch(query)
         .then(res => res.json())
         .then(json => { 
         if (json.hasOwnProperty('total')) {
-          statsIssues(json.total, rules[idrule]);
-          fetchIssues(idrule + 1, type, resolution);
+          var whereiam = json.p * json.ps;
+          
+          json.issues.forEach(function(element) {
+              if((element.fromHotspot && fromhotspot == "true") || (!element.fromHotspot && fromhotspot == "false") ) {
+                totalsofar ++;
+              }
+          });
+        
+          if(json.total > whereiam && fromhotspot !== "notdefined") {
+            fetchIssues(page + 1, idrule, issuetypes, resolution, statuses, fromhotspot, totalsofar);
+          }
+          else {
+            // end
+            if(fromhotspot !== "notdefined") {
+              statsIssues(totalsofar, rules[idrule]);
+            }
+            else {
+              statsIssues(json.total, rules[idrule]);
+            }
+            
+            fetchIssues(1, idrule + 1, issuetypes, resolution, statuses, fromhotspot, 0);
+          }
+        }
+        else {
+          if(fromhotspot !== "notdefined") {
+            statsIssues(totalsofar, rules[idrule]);
+          }
+          else {
+            statsIssues(0, rules[idrule]);
+          }
+            
+          fetchIssues(1, idrule + 1, issuetypes, resolution, statuses, fromhotspot, 0);
         }
     });
   }
@@ -51,7 +97,8 @@ function statsRules(json) {
           "name": element.name,
           "language": element.key.split(":")[0],
           "rspecnum":element.key.slice(-4),
-          "id": element.key
+          "id": element.key,
+          "type": element.type
         };
         
         rules[id] = obj;
@@ -60,9 +107,13 @@ function statsRules(json) {
   }
 }
 
-function fetchRules(page, type, resolution) {
+function fetchRules(page, ruletypes, issuetypes, resolution, statuses="OPEN,CONFIRMED,REOPENED,RESOLVED,CLOSED,TO_REVIEW,IN_REVIEW,REVIEWED", fromhotspot="notdefined") {
   // first page
-  fetch('https://sonarcloud.io/api/rules/search?types='+type+'&ps=500&p='+page)
+  if(statuses === "ALL") {
+    statuses = "OPEN,CONFIRMED,REOPENED,RESOLVED,CLOSED,TO_REVIEW,IN_REVIEW,REVIEWED";
+  }
+  
+  fetch('https://sonarcloud.io/api/rules/search?types='+ruletypes+'&ps=500&p='+page)
       .then(res => res.json())
       .then(json => { 
         
@@ -70,24 +121,31 @@ function fetchRules(page, type, resolution) {
         var whereiam = json.p * json.ps;
         
         if(json.total > whereiam) {
-            fetchRules(page + 1, type, resolution);
+            fetchRules(page + 1, ruletypes, issuetypes, resolution, statuses);
         }
         else {
           // end
-          fetchIssues(0, type, resolution);
+          fetchIssues(1, 0, issuetypes, resolution, statuses, fromhotspot);
         }
     });
 }
-
-if(process.argv.length < 4) {
-  console.log("node stats.js type resolution");
-  console.log("where type could be: VULNERABILITY, CODE_SMELL, SECURITY_HOTSPOT or BUG");
-  console.log("where resolution could be: WONTFIX, FALSE-POSITIVE, FIXED or REMOVED");
+    
+if(process.argv.length < 5 || process.argv.length > 8) {
+  console.log("node stats.js type resolution statuses fromhotspot");
+  console.log("where rule type could be: VULNERABILITY,CODE_SMELL,SECURITY_HOTSPOT,BUG");
+  console.log("where issue type could be: VULNERABILITY,CODE_SMELL,SECURITY_HOTSPOT,BUG");
+  console.log("where resolution could be: ALL,NOTRESOLVED,WONTFIX,FALSE-POSITIVE,FIXED,REMOVED");
+  console.log("where statuses is optional and could be: ALL,OPEN,CONFIRMED,REOPENED,RESOLVED,CLOSED,TO_REVIEW,IN_REVIEW,REVIEWED");
+  console.log("where fromhotspot is optional and could be: false,true");
 }   
-else {
-  fetchRules(1, process.argv[2], process.argv[3]);
+else if(process.argv.length == 5) {
+  fetchRules(1, process.argv[2], process.argv[3], process.argv[4]);
 }
-
-
+else if(process.argv.length == 6) {
+  fetchRules(1, process.argv[2], process.argv[3], process.argv[4], process.argv[5]);
+}
+else if(process.argv.length == 7) {
+  fetchRules(1, process.argv[2], process.argv[3], process.argv[4], process.argv[5], process.argv[6]);
+}
 
 
